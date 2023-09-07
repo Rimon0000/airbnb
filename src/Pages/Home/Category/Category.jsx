661a96321b4681e3c8594fd2e9ce3ec972c9b7c5
Your Name <Rimon0000>
2023-09-07 22:33:02 +0600

    show category wise data

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -7,69 +7,102 @@ import { GiHighGrass, GiHomeGarage, GiPalmTree, GiSkeletonKey, GiUmbrella, GiWoo
 
 const Category = () => {
     const [allCategory, setAllCategory] = useState([])
-    const [tabValue, setTabValue] = useState("marvel")
+    const [tabValue, setTabValue] = useState("Rooms")
+    
 
-  useEffect(() =>{
-    fetch(`https://toy-server-theta.vercel.app/allCategorys/${tabValue}`)
-    .then(res => res.json())
-    .then(data => setAllCategory(data))
-  },[tabValue])
+   useEffect(() =>{
+     fetch(`http://localhost:5000/allServices/${tabValue}`)
+     .then(res => res.json())
+     .then(data => setAllCategory(data))
+   },[tabValue])
 
     return (
         <div>
             <h2 className='font-semibold text-2xl text-center m-5 p-5'>Our Categories</h2>
             <Tabs>
                <TabList className='text-center font-semibold mb-5'>
-                 <Tab onClick={() => setTabValue("marvel")}>
+                 <Tab onClick={() => setTabValue("Rooms")}>
                  <div className="ml-4"><GiHomeGarage/></div>
                     Rooms</Tab>
-                 <Tab onClick={() => setTabValue("avengers")}>
+                 <Tab onClick={() => setTabValue("Countryside")}>
                  <div className="ml-8"><GiHighGrass/></div>
                     CountrySide</Tab>
-                 <Tab onClick={() => setTabValue("star wars")}>
+                 <Tab onClick={() => setTabValue("Tropical")}>
                  <div className="ml-4"><GiPalmTree/></div>
                     tropical</Tab>
-                 <Tab onClick={() => setTabValue("star wars")}>
+                 <Tab onClick={() => setTabValue("New")}>
                     <div className="ml-3"><GiSkeletonKey/></div>
                     New</Tab>
-                 <Tab onClick={() => setTabValue("star wars")}>
+                 <Tab onClick={() => setTabValue("Cabins")}>
                  <div className="ml-4"><GiWoodCabin/></div>
                     Cabins</Tab>
-                 <Tab onClick={() => setTabValue("star wars")}>
+                 <Tab onClick={() => setTabValue("Beach")}>
                  <div className="ml-4"><GiUmbrella/></div>
                     Beach</Tab>
                </TabList>
            
                <TabPanel>
-                 <div className='grid md:grid-cols-3 sm:grid-cols-1'>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
                  {
-                  allCategory.map(toy => <SubCategory 
-                    key={toy._id}
-                    toy={toy}
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
                     ></SubCategory>)
                  }
                  </div>
                </TabPanel>
                <TabPanel>
-               <div className='grid md:grid-cols-3 sm:grid-cols-1'>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
                  {
-                  allCategory.map(toy => <SubCategory 
-                    key={toy._id}
-                    toy={toy}
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
                     ></SubCategory>)
                  }
                  </div>
                </TabPanel>
                <TabPanel>
-               <div className='grid md:grid-cols-3 sm:grid-cols-1'>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
                  {
-                  allCategory.map(toy => <SubCategory 
-                    key={toy._id}
-                    toy={toy}
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
                     ></SubCategory>)
                  }
                  </div>
                </TabPanel>
+               <TabPanel>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
+                 {
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
+                    ></SubCategory>)
+                 }
+                 </div>
+               </TabPanel>
+               <TabPanel>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
+                 {
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
+                    ></SubCategory>)
+                 }
+                 </div>
+               </TabPanel>
+               <TabPanel>
+                 <div className='grid md:grid-cols-4 sm:grid-cols-1'>
+                 {
+                  allCategory.map(item => <SubCategory 
+                    key={item._id}
+                    item={item}
+                    ></SubCategory>)
+                 }
+                 </div>
+               </TabPanel>
+             
+              
             </Tabs>
             
         </div>
--- src/Pages/Home/SubCategory/SubCategory.jsx
+++ src/Pages/Home/SubCategory/SubCategory.jsx
@@ -1,9 +1,18 @@
 
 
-const SubCategory = () => {
+const SubCategory = ( {item} ) => {
+    console.log(item)
+    const {photo, location, rating, price} = item
     return (
-        <div>
-            <h2>This is sub category</h2>
+        <div className="card card-compact mx-auto w-96 bg-base-100 shadow-xl px-5">
+          <figure><img src={photo} alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Location: {location}</h2>
+            <div className='font-semibold'>
+            <p className='py-1'>price: {price}</p>
+            <p className='py-1 pb-2'>Rating: { rating}</p>
+            </div>
+          </div>
         </div>
     );
 };
