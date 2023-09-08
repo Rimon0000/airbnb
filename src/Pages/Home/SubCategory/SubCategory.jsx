b40f841192734f02ba5e828ffe40ae47d8800a83
Your Name <Rimon0000>
2023-09-08 13:27:06 +0600

    modify the design

--- src/Pages/Home/SubCategory/SubCategory.jsx
+++ src/Pages/Home/SubCategory/SubCategory.jsx
@@ -1,16 +1,25 @@
-
+import { FaStar } from "react-icons/fa";
 
 const SubCategory = ( {item} ) => {
     console.log(item)
-    const {photo, location, rating, price} = item
+    const {photo, location, rating, price, description, date} = item
     return (
-        <div className="card card-compact mx-auto w-96 bg-base-100 shadow-xl px-5">
-          <figure><img src={photo} alt="Shoes" /></figure>
+        <div className="card card-compact mx-auto bg-base-100 px-5">
+          <figure className="rounded-lg"><img src={photo} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Location: {location}</h2>
-            <div className='font-semibold'>
-            <p className='py-1'>price: {price}</p>
-            <p className='py-1 pb-2'>Rating: { rating}</p>
+            <div className="flex justify-between items-center">
+              <div>
+              <h2 className="text-base font-semibold">Location: {location}</h2>
+              </div>
+              <div className="flex items-center gap-1">
+                <p><FaStar/></p>
+                <p>{rating}</p>
+              </div>
+            </div>
+            <div className=''>
+              <p className="text-base text-gray-500">{description}</p>
+              <p className="text-base text-gray-500">{date}</p>
+            <p className='py-1 text-base'><span className="font-semibold">${price}</span> night</p>
             </div>
           </div>
         </div>
